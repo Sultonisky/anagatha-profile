2ec1f1add7e846a372f43a9cd2abda157165b4d5
sultonisky <muhsultonipml111@gmail.com>
2025-12-02 17:24:56 +0700

    fix: enhanced to represent dashboard

--- resources/views/admin/admin_layouts/partials/footer.blade.php
+++ resources/views/admin/admin_layouts/partials/footer.blade.php
@@ -1,16 +1,13 @@
-<footer class="footer">
+<footer class="footer bg-white border-top">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6">
-                <script>
-                    document.write(new Date().getFullYear())
-                </script> © Upcube.
-            </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesdesign
-                </div>
+            <div class="col-12 text-center">
+                <span class="text-muted">
+                    © <script>document.write(new Date().getFullYear())</script>
+                    <strong>Anagata Executive</strong> Dashboard
+                </span>
             </div>
         </div>
     </div>
 </footer>
+
